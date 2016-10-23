c412e7c54a3a72316deb982f802d999596b45230
Manuel Guarniz <cruzemg95@gmail.com>
2016-10-23 00:25:24 -0500

    cambios
    cambios en la vista, plantillas, bootstrap, agregando lineas de
    comandos, configurando urls

--- refugio/urls.py
+++ refugio/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^mascota/', include('apps.mascota.urls'), namesace="mascota"),
-    url(r'^adopcion/', include('apps.adopcion.urls'), namespace="adopcion"),
+    url(r'^mascota/', include('apps.mascota.urls', namespace="mascota")),
+    url(r'^adopcion/', include('apps.adopcion.urls', namespace="adopcion")),
 ]
