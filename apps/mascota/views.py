f644c973e524e6fd70227287ad8446374043c7e0
Manuel Guarniz <cruzemg95@gmail.com>
2016-10-29 22:06:07 -0500

    [Push] editar y eliminar
    creando las vistas para eliminar, agregando al listado un orden por
    "id", agregando botones editar y eliminar, permitir el guardado de la
    edicion, permitir la eliminacion, comentando codigo para explicacion del
    mismo

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -8,17 +8,40 @@ from apps.mascota.models import Mascota
 def index(request):
     return render(request, 'mascota/index.html')
 
+# Crea formulario para registrar una mascota
 def mascota_view(request):
     if request.method == 'POST':
+        # Recoge los datos ingresados y estos son guardados en form
         form = MascotaForm(request.POST)
-        if form.is_valid():
+        if form.is_valid(): # Valida los datos ingresados
             form.save()
-        return redirect('mascota:index')
+        return redirect('mascota:mascota_listar')
     else:
         form = MascotaForm()
     return render(request, 'mascota/mascota_form.html', {'form': form})
 
+# Retorna un listado de todos los registros
 def mascota_list(request):
-	mascota = Mascota.objects.all()
+	mascota = Mascota.objects.all().order_by('id')
 	contexto = {'mascotas':mascota}
 	return render(request, 'mascota/mascota_list.html', contexto)
+
+# Funcion para realizar la ediccion, necesita el "id" de la mascota seleccionada
+def mascota_edit(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'GET':
+        form = MascotaForm(instance=mascota)
+    else:
+        form = MascotaForm(request.POST, instance=mascota)
+        if form.is_valid(): # Valida los datos ingresados
+            form.save()
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_form.html',{'form':form})
+
+# Funcion para eliminar un registro, necesita un "id" para la eliminacion
+def mascota_delete(request, id_mascota):
+    mascota = Mascota.objects.get(id=id_mascota)
+    if request.method == 'POST':
+        mascota.delete()
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_delete.html',{'mascota':mascota})
