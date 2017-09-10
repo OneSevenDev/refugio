de414a9626a6131343f9d9b7f3299d4fedad4269
Manuel Guarniz <cruzemg95@gmail.com>
2017-09-10 02:55:01 -0500

    remove directories innecesary

--- apps/adopcion/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from apps.adopcion.views import index_adopcion
-
-urlpatterns = [
-    url(r'^index$', index_adopcion)
-]
--- apps/adopcion/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index_adopcion(request):
-    return HttpResponse("Pagina de adopcion")
